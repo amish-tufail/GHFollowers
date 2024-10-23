f7eda28dd29e3c53c5e51917e539847646374ad1
amishtufail11 <162984629+amishtufail11@users.noreply.github.com>
2024-10-23 07:09:01 +0500

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Amish on 21/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.cyan
+    }
+
+
+}
+
