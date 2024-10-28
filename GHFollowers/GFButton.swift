6756a9b0843fc5950389a456de209030df04e0d9
amishtufail11 <162984629+amishtufail11@users.noreply.github.com>
2024-10-28 20:22:35 +0500

    if applied, this will create GFButton, GFTextField and then implement the UI of Search Screen

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Amish on 23/10/2024.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    // We override only when we do something custom
+    override init(frame: CGRect) {
+        super.init(frame: frame) // Default init -> Parent -> All Apple written
+        // Then we do our custom one
+        configure()
+    }
+    
+    // This gets called when we init using storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10.0
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -7,11 +7,64 @@
 
 import UIKit
 
-class SearchVC: UIViewController {
+// RULE OF THUMB: Each object needs 4 constraints to where layout in view: X, Y, Width & Height. Traling & Bottom edges will have negative values
 
+class SearchVC: UIViewController {
+    let logoImageView = UIImageView()
+    let searchTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+        configure()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+    
+    
+    private func configure() {
+        configureLogoImageView()
+        configureSearchTextField()
+        configureCallToActionButton()
+    }
+    
+    private func configureLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(resource: .ghLogo)
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80.0),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200.0),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200.0),
+        ])
+    }
+    
+    private func configureSearchTextField() {
+        view.addSubview(searchTextField)
+        
+        NSLayoutConstraint.activate([
+            searchTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48.0),
+            searchTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50.0),
+            searchTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50.0),
+            searchTextField.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+            
+    }
+    
+    private func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50.0),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50.0),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50.0)
+        ])
+    }
 }
