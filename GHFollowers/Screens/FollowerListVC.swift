6e15dd26b70eb9fea8cd9ab6ea3b0066e545f497
amish-tufail <amish.tufail2002@gmail.com>
2024-11-13 10:25:05 +0500

    add pagination

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -18,11 +18,14 @@ class FollowerListVC: UIViewController {
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
+    var page: Int = 1
+    var hasMoreFollowers: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureVC()
         configureCollectionView()
-        getFollowers()
+        getFollowers(for: userName)
         configureDataSource()
     }
     
@@ -40,18 +43,22 @@ class FollowerListVC: UIViewController {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self 
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseIdentifier)
     }
 
-    private func getFollowers() {
-        NetworkManager.shared.getFollowers(for: userName, page: 1) { [weak self] result in
+    private func getFollowers(for userName: String, page: Int = 1) {
+        NetworkManager.shared.getFollowers(for: userName, page: page) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let followers):
                 print("Followers Count: \(followers.count)")
                 print("Followers:")
                 print(followers)
-                self.followers = followers
+                if followers.count < 100 {
+                    hasMoreFollowers = false
+                }
+                self.followers.append(contentsOf: followers)
                 self.configureSnapshot()
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, actionTitle: "Ok")
@@ -67,13 +74,27 @@ class FollowerListVC: UIViewController {
         }
     }
     
-    private func configureSnapshot() {
+    private func  configureSnapshot() {
         var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
         
         DispatchQueue.main.async {
             self.dataSource.apply(snapshot, animatingDifferences: true)
+        }
+    }
+}
+
+extension FollowerListVC: UICollectionViewDelegate {
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.size.height
+        
+        if offsetY > contentHeight - height {
+            guard hasMoreFollowers else { return }
+            page += 1
+            self.getFollowers(for: userName, page: page)
         }
     }
 }
